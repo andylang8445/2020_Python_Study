c758e6263cb391c29dc75486996802408877c1a7
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-08-08 02:11:08 -0400

    Update main.py

--- 20200807/main.py
+++ 20200807/main.py
@@ -1,8 +1,19 @@
-print("직각삼각형 그리기\n")
+'''print("직각삼각형 그리기\n")
 leg=int(input("변의 길이: "))
 
 for i in range(leg):
     print("* "*(i+1))
 
 area=(leg**2)/2
-print("넓이: %g"%area)
+print("넓이: %g"%area)'''
+
+'''num=1
+while num<=100:
+    print(num)
+    num+=1'''
+
+num=int(input())
+i=1
+while i<=num:
+    print(i," ",i**2)
+    i+=1
