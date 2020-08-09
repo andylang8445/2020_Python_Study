00773ef5c86b584f52ee13e8f821064726c1306d
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-08-08 21:20:38 -0400

    Update main.py

--- 202020808/main.py
+++ 202020808/main.py
@@ -1,4 +1,4 @@
-a = 1
+'''a = 1
 b = 2
 c = 3
 d = 4
@@ -7,4 +7,24 @@ if a > b:
 elif a == b:
     print('equal')
 else:
-    print('b')
+    print('b')'''
+
+'''current=int(input())
+check=0
+
+if current%4==0:
+    if current%100==0:
+        if current%400==0:
+            check=1
+    else:
+        check=1
+
+if check==1:
+    print("윤년!")
+else:
+    prin("평년")'''
+
+family = ['mother', 'father', 'me', 'sister']
+
+for x in family:
+    print('%s %d'%(x,len(x)))
